ad785b196aa4a3d13b0c46a107567bcec36c9f6e
Rahul9198 <rahulkumarverma9198@gmail.com>
2024-06-14 16:54:28 +0530

    Servlet and Jsp

---
+++ Eclipse_IDE/MyAppProject/src/main/java/com/rahul/SqServlet.java
@@ -0,0 +1,9 @@
+package com.rahul;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet HttpServlet{
+	public void doGet(HttpServletRequest req,HttpServletResponse res)
+
+}
---
+++ Eclipse_IDE/MyConnection/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MyConnection {
+	requires java.sql;
+}
